6a5be22b4fe893214b60106356e81c3730f0ce9c
Siddharth Matia <siddharthmatia@chatham-nj.org>
2024-12-03 15:08:14 -0500

    Add - Initial RoadRunnerTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class RoadRunnerTest extends LinearOpMode {
+    private MecanumDrive robot = new MecanumDrive(hardwareMap, new Pose2d(0,0, 0));
+    @Override
+    public void runOpMode() {
+    }
+}
